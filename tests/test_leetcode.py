593633ae9885784c814efbc8b375d93da430cc65
Kent Chiu <kent.cwg@gmail.com>
2022-04-15 08:57:43 +0800

    remove duplicate done

--- tests/test_leetcode.py
+++ tests/test_leetcode.py
@@ -1,20 +1,36 @@
-from leetcode import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.1'
-
-def test_remove_duplicates_from_sorted_array():
-    nums =[1,1,2]
-    expected_nums = [1,2]
+def test_remove_duplicates_from_sorted_array_1():
+    nums = [1, 1, 2]
+    expected_nums = [1, 2]
     k = remove_duplicates(nums)
 
-    assert 2 == len(k)
+    assert k == len(expected_nums)
 
-    i = 0
-    for expected in expected_nums:
-        assert k[i] == expected;
-        i += 1
+    for i, expected in enumerate(expected_nums):
+        assert nums[i] == expected;
 
-def remove_duplicates(nums):
-    return [1,2]
+
+def test_remove_duplicates_from_sorted_array_2():
+    nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    expected_nums = [0, 1, 2, 3, 4]
+    k = remove_duplicates(nums)
+
+    assert k == len(expected_nums)
+
+    for i, expected in enumerate(expected_nums):
+        assert nums[i] == expected;
+
+
+def remove_duplicates(nums: list[int]) -> int:
+    p1 = 0
+    p2 = 1
+    while p2 <= len(nums) - 1:
+        prev_num = nums[p1]
+        current_num = nums[p2]
+        if current_num != prev_num:
+            p1 += 1
+            p2 += 1
+        else:
+            nums.pop(p2)
+    return len(nums)
+
+
