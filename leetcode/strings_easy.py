1d83798f5f9006997a36046d50344d6ca0549ce2
Kent Chiu <kent.cwg@gmail.com>
2022-04-16 16:19:48 +0800

    first uniq char

--- leetcode/strings_easy.py
+++ leetcode/strings_easy.py
@@ -1,20 +1,48 @@
 #  Reverse String, First Unique Character in a String, String to Integer (atoi) and Implement strStr().
 
+
+"""
+cost: 90 minutes, 97%
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+"""
+
+
+def first_uniq_char(s: str) -> int:
+    ## shortcircuit for single character
+    if len(s) == 1:
+        return 0
+    duplicates = set()
+    for idx, c in enumerate(s):
+        if idx > len(s) - 1:
+            return -1
+
+        #  bypass duplicated characters
+        if c not in duplicates:
+            find = s.find(c, idx + 1)
+            if find != -1:
+                duplicates.add(c)
+            else:
+                return idx
+    return -1
+
+
 """
 cost: 45 minutes, 78%
 Write a function that reverses a string. The input string is given as an array of characters s.
 You must do this by modifying the input array in-place with O(1) extra memory.
 """
+
+
 def reverse_string(s: list[str]) -> None:
     """
     Do not return anything, modify s in-place instead.
     """
     s.insert(0, ' ')
-    tail = len(s) -1
+    tail = len(s) - 1
     head = 0
-    for k in range(0 , len(s) // 2):
+    for k in range(0, len(s) // 2):
         s[head] = s[tail]
-        s[tail] = s[head +1]
+        s[tail] = s[head + 1]
         head += 1
         tail -= 1
     s.pop(head)
--- tests/test_strings_easy.py
+++ tests/test_strings_easy.py
@@ -1,4 +1,12 @@
-from leetcode.strings_easy import reverse_string
+from leetcode.strings_easy import reverse_string, first_uniq_char
+
+
+def test_first_uniq_char():
+    assert first_uniq_char("dddccdbba") == 8
+    assert first_uniq_char("leetcode") == 0
+    assert first_uniq_char("loveleetcode") == 2
+    assert first_uniq_char("aabb") == -1
+    assert first_uniq_char("z") == 0
 
 
 def test_rotate():
@@ -9,7 +17,3 @@ def test_rotate():
     s = ["H", "a", "n", "n", "a", "h"]
     reverse_string(s)
     assert s == ["h", "a", "n", "n", "a", "H"]
-
-    s = ["1", "2", "3", "4", "5", "6", "7","8","9"]
-    reverse_string(s)
-    print(f'---->{s}')
