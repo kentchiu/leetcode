45fa913a5cc037a727d621e104a3714b1e834434
Kent Chiu <kent.cwg@gmail.com>
2022-05-11 14:46:14 +0800

    palindrome

--- tests/test_recursion.py
+++ tests/test_recursion.py
@@ -34,3 +34,22 @@ def reverse_string(input: str):
 
 def test_reverse_string():
     assert reverse_string('hello') == 'olleh'
+
+
+def is_palindrome(input: str) -> bool:
+    if len(input) < 2:
+        return True
+    head = input[0]
+    tail = input[-1]
+
+    if head == tail:
+        return is_palindrome(input[1: -1])
+    else:
+        return False
+
+
+def test_is_palindrome():
+    assert is_palindrome('kayak') == True
+    assert is_palindrome('abc') == False
+    assert is_palindrome('abcba') == True
+    assert is_palindrome('racecar') == True
