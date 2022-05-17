52c0f2ae5610a22b5fc30051682ae6f996120ce4
Kent Chiu <kent.cwg@gmail.com>
2022-05-17 22:13:55 +0800

    climb_stairs

--- tests/test_dynamic_programming_easy.py
+++ tests/test_dynamic_programming_easy.py
@@ -5,13 +5,18 @@
 #  House Robber
 
 def climb_stairs(n: int) -> int:
-    # fn = f(n-1) + f(n-2)
-    if n == 1:
-        return 1
-    if n == 2:
-        return 2
 
-    return climb_stairs(n - 1) + climb_stairs(n - 2)
+    def dfs(i: int, memo: dict) -> int:
+        # 使用 dict.get() 在 key 不存在時, 才不會報錯
+        if memo.get(i):
+            return memo[i]
+
+        result = dfs(i - 1, memo) + dfs(i - 2, memo)
+        memo[i] = result
+        return result
+
+    memo: dict = {1: 1, 2: 2}
+    return dfs(n, memo)
 
 
 def test_climb_stairs():
