7505f6369c08b5e1991f42f738daaf841c5e630f
Kent Chiu <kent.cwg@gmail.com>
2022-05-17 22:48:05 +0800

    max profit

--- tests/test_dynamic_programming_easy.py
+++ tests/test_dynamic_programming_easy.py
@@ -4,8 +4,11 @@
 #  Maximum Subarray
 #  House Robber
 
-def climb_stairs(n: int) -> int:
 
+from typing import List
+
+
+def climb_stairs(n: int) -> int:
     def dfs(i: int, memo: dict) -> int:
         # 使用 dict.get() 在 key 不存在時, 才不會報錯
         if memo.get(i):
@@ -25,3 +28,21 @@ def test_climb_stairs():
     assert climb_stairs(3) == 3
     assert climb_stairs(4) == 5
     assert climb_stairs(5) == 8
+
+
+def max_profit(prices: List[int]) -> int:
+    # 沒有到 DP
+    # out of time
+    max = 0
+    for i, p1 in enumerate(prices):
+        for j, p2 in enumerate(prices[i:]):
+            print(f'i:{i}->{p1}, j:{j}->{p2}')
+            profit = p2 - p1
+            if profit > max:
+                max = profit
+    return max
+
+
+def test_max_profit():
+    assert max_profit([7, 1, 5, 3, 6, 3]) == 5
+    assert max_profit([7, 6, 4, 3, 1]) == 0
