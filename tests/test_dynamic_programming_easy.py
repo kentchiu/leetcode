09cb1f4d2a9dc57880caff427e2292a6dcfeb5d2
Kent Chiu <kent.cwg@gmail.com>
2022-05-19 00:59:41 +0800

    max_profit

--- tests/test_dynamic_programming_easy.py
+++ tests/test_dynamic_programming_easy.py
@@ -45,6 +45,14 @@ def max_profit_timeout(prices: List[int]) -> int:
 
 
 def max_profit(prices: List[int]) -> int:
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        min_price = min(min_price, price)
+        max_profit = max(max_profit, price - min_price)
+
+    return max_profit
 
 
 def test_max_profit():
