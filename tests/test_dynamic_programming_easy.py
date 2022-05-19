a22ea866a34452beedd6a9fbef02bd32c1a4dbbe
Kent Chiu <kent.cwg@gmail.com>
2022-05-19 13:51:11 +0800

    dp

--- tests/test_dynamic_programming_easy.py
+++ tests/test_dynamic_programming_easy.py
@@ -7,20 +7,7 @@
 
 from typing import List
 
-
-def climb_stairs(n: int) -> int:
-    # 沒有用到 DP, DP通常會使用 iteration 而不是 recursion
-    def dfs(i: int, memo: dict) -> int:
-        # 使用 dict.get() 在 key 不存在時, 才不會報錯
-        if memo.get(i):
-            return memo[i]
-
-        result = dfs(i - 1, memo) + dfs(i - 2, memo)
-        memo[i] = result
-        return result
-
-    memo: dict = {1: 1, 2: 2}
-    return dfs(n, memo)
+from leetcode.dynamic_programming_easy import climb_stairs, max_profit
 
 
 def test_climb_stairs():
@@ -29,30 +16,6 @@ def test_climb_stairs():
     assert climb_stairs(3) == 3
     assert climb_stairs(4) == 5
     assert climb_stairs(5) == 8
-
-
-def max_profit_timeout(prices: List[int]) -> int:
-    # 沒有到 DP
-    # out of time
-    max = 0
-    for i, p1 in enumerate(prices):
-        for j, p2 in enumerate(prices[i:]):
-            print(f'i:{i}->{p1}, j:{j}->{p2}')
-            profit = p2 - p1
-            if profit > max:
-                max = profit
-    return max
-
-
-def max_profit(prices: List[int]) -> int:
-    min_price = float('inf')
-    max_profit = 0
-
-    for price in prices:
-        min_price = min(min_price, price)
-        max_profit = max(max_profit, price - min_price)
-
-    return max_profit
 
 
 def test_max_profit():
